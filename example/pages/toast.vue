c0fe2679b2eb5dc68cbdd33df5df97679cdac422
zhuhongyan <zhuhyb@sunits.com>
2018-02-05 21:59:38 +0800

    toast提示文字更改

--- example/pages/toast.vue
+++ example/pages/toast.vue
@@ -8,15 +8,15 @@
         </div>
 
         <toast v-model="showPositionValue" type="text" :time="800" is-show-mask text="Hello World" :position="position">
-            {{ ('Basic Usage') }}
+            {{ '基本使用' }}
         </toast>
         <group>
             <x-switch :title="'一般使用'" v-model="show1"></x-switch>
-            <x-switch :title="('type为text')" v-model="show2"></x-switch>
-            <x-switch :title="('type为cancel')" v-model="show3"></x-switch>
-            <x-switch :title="('type为warn')" v-model="show4"></x-switch>
-            <x-switch :title="('显示时间一秒')" v-model="show5"></x-switch>
-            <x-switch :title="('很长文字')" v-model="show6"></x-switch>
+            <x-switch :title="'type为text'" v-model="show2"></x-switch>
+            <x-switch :title="'type为cancel'" v-model="show3"></x-switch>
+            <x-switch :title="'type为warn'" v-model="show4"></x-switch>
+            <x-switch :title="'显示时间一秒'" v-model="show5"></x-switch>
+            <x-switch :title="'很长文字'" v-model="show6"></x-switch>
         </group>
 
         <toast v-model="show1" @on-hide="onHide">{{ ('Basic Usage') }}</toast>
@@ -27,8 +27,8 @@
         <toast v-model="show6" type="text" width="20em">{{('show me code')}}</toast>
 
         <group :title="('prop:text')">
-            <x-switch :title="('type为success')" v-model="show7"></x-switch>
-            <x-switch :title="('type为text')" v-model="show8"></x-switch>
+            <x-switch :title="'type为success'" v-model="show7"></x-switch>
+            <x-switch :title="'type为text'" v-model="show8"></x-switch>
         </group>
 
         <toast v-model="show7" text="Hello World"></toast>
