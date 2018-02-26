425eca520a650b5cb221a1a18ac406eb7c047112
W.Hao <wanghao29@lenovo.com>
2018-02-26 18:25:43 +0800

    init docs page

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
     entry: {
-        app: './example/main.js'
+        app: './example/main.js',
+        doc: './document/main.js'
     },
     module: {
         rules: utils.styleLoaders({sourceMap: config.dev.cssSourceMap, usePostCSS: true})
@@ -59,6 +60,13 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './example/index.html',
+            chunks: ['app', 'vendor', 'manifest'],
+            inject: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'document.html',
+            template: './document/index.html',
+            chunks: ['doc', 'vendor', 'manifest'],
             inject: true
         }),
         // copy custom static assets
---
+++ document/docs/demo.md
@@ -0,0 +1,46 @@
+## 组件名称
+
+组件介绍
+
+### 示例
+
+- 例子1
+```javascript
+let docRoutes = []
+menu.forEach(({children}) => {
+    children.forEach(({name}) => {
+        docRoutes.push({
+            path: `${name}`,
+            name: `doc-${name}`,
+            component: resolve => require([`../docs/${name}.md`], resolve)
+        })
+    })
+})
+```
+
+- 例子2
+```javascript
+const router = new VueRouter({
+    linkActiveClass: 'active',
+    routes,
+    scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { x: 0, y: 0 }
+        }
+    }
+})
+```
+
+### Props 配置
+
+| 参数 | 说明 | 类型 | 可选值 | 默认值 |
+| - | - | - | - | - |
+| title | 组件的标题 | String | - | '' |
+
+### 事件
+
+| 参数 | 说明 | 参数1 | 参数2 |
+| - | - | - | - |
+| cancel | 点击取消 | - | - |
